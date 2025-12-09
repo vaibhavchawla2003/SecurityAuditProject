0f8fe471735aedc2f7ab155bcb429813270f28b5
vaibhavchawla2003 <vaibhavchawla26315252@gmail.com>
2025-12-09 10:25:01 +0530

    madarchod

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import App from "./App";
+import App from "./App";   // <-- Only this is correct
 
 const container = document.getElementById("root");
 const root = createRoot(container);
